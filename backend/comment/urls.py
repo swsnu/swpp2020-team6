575a67ef02b437c36cf357d67c660c9e14fbb79b
ddanddan18 <bob070030@gmail.com>
2020-11-06 22:30:45 +0900

    feat: add urls

---
+++ backend/comment/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from comment import views
+
+urlpatterns = [
+    path("", views.comment, name="comment"),
+    path("<int:comment_id>/", views.comment_id, name="comment_id"),
+]
