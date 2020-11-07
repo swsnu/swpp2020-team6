6980f7719a531777c953b41097effd5500be0590
gina7484 <gina7484@naver.com>
2020-11-07 14:36:12 +0900

    feat: defined constants.
    Defined constants.
    
    How to user (example)
        import { taskType, levelType, progressState } from "../../constants";
    
        const roadmapSample = {
        level: levelType.BASIC,
        ...
        progress: progressState.BEFORE_STUDY,
        author: 1,
        }

---
+++ frontend/src/constants.js
@@ -0,0 +1,18 @@
+export const taskType = {
+  BOOK: 1,
+  WEBSITE: 2,
+  VIDEO: 3,
+  PAPER: 4,
+};
+
+export const levelType = {
+  BASIC: 1,
+  INTERMEDIATE: 2,
+  ADVANCED: 3,
+};
+
+export const progressState = {
+  BEFORE_STUDY: 1,
+  IN_PROGRESS: 2,
+  FINISHED: 3,
+};
