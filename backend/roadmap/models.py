cc9065f8ec510b0adbe209c601fa8559645be736
ddanddan18 <bob070030@gmail.com>
2020-11-09 01:07:23 +0900

    feat: add Roadmap to Dict object function Function to make default roadmap dictionary object

--- backend/roadmap/models.py
+++ backend/roadmap/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from section.models import Section
 from tag.models import Tag
+from itertools import chain
+from user.models import User
 
 
 class Roadmap(models.Model):
     title = models.CharField(max_length=64, default="")
-    date = models.DateField(blank=True)
+    date = models.DateTimeField(blank=True)
     level = models.IntegerField(default=0)
     like_count = models.IntegerField(default=0)
     comment_count = models.IntegerField(default=0)
@@ -23,3 +25,63 @@ class Roadmap(models.Model):
         Section, related_name="section_roadmap", blank=True
     )
     tags = models.ManyToManyField(Tag, related_name="tags_roadmap", blank=True)
+
+    def __str__(self):
+        return "{}".format(self.title)
+
+    def to_roadmap_object(self):
+        """
+        :return: Roadmap Object Dictionary (contain author/tags/sections/tasks/comments info)
+        """
+        options = self._meta
+        data = {}
+        for f in chain(options.concrete_fields):
+            if f.name == "date":
+                data[f.name] = f.value_from_object(self).strftime("%Y-%m-%d %H:%M:%S")
+            elif f.name == "author":
+                author = User.objects.get(id=f.value_from_object(self))
+                data["author_id"] = author.id
+                data["author_name"] = author.username
+                data["author_user_picture_url"] = author.user_picture_url
+            elif f.name == "original_author":
+                original_author = User.objects.get(id=f.value_from_object(self))
+                data["original_author"] = original_author.id
+                data["original_author_name"] = original_author.username
+            else:
+                data[f.name] = f.value_from_object(self)
+
+        for f in chain(options.many_to_many):
+            if f.name == "tags":
+                data[f.name] = list(
+                    {"tag_id": tag.id, "tag_name": tag.tag_name}
+                    for tag in f.value_from_object(self)
+                )
+            elif f.name == "sections":
+                data[f.name] = list(
+                    {
+                        "section_title": section.title,
+                        "tasks": list(
+                            {
+                                "task_title": task.title,
+                                "task_type": task.type,
+                                "task_url": task.url,
+                                "task_description": task.description,
+                                "task_checked": task.checked,
+                            }
+                            for task in section.tasks.all()
+                        ),
+                    }
+                    for section in f.value_from_object(self)
+                )
+
+        data["comments"] = list(
+            {
+                "roadmap_id": comment.roadmap_id,
+                "content": comment.content,
+                "author_id": comment.author_id,
+                "author_name": comment.author.username,
+                "author_picture_url": comment.author.user_picture_url,
+            }
+            for comment in self.roadmap_comment.all()
+        )
+        return data
