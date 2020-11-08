7fd80313a61ee015d6bf7da07e58200c9bd98cb4
ddanddan18 <bob070030@gmail.com>
2020-11-09 01:12:59 +0900

    feat: add migration file, model_to_dict default version
    Not use model_to_dict directly, I customize it like roadmap/models.py function

--- backend/utils/model_to_dict.py
+++ backend/utils/model_to_dict.py
@@ -1,4 +1,5 @@
 from django.db.models.fields.related import ManyToManyField
+from itertools import chain
 
 
 def to_dict(instance):
@@ -8,14 +9,16 @@ def to_dict(instance):
     """
     options = instance._meta
     data = {}
-    for f in options.concrete_fields + options.many_to_many:
+    print(options.concrete_fields + options.many_to_many)
+    for f in chain(options.concrete_fields + options.many_to_many):
         if isinstance(f, ManyToManyField):
+            print(instance.pk, f.name, f.value_from_object(instance))
             if instance.pk is None:
                 data[f.name] = []
             else:
-                data[f.name] = list(
-                    f.value_from_object(instance).values_list("pk", flat=True)
-                )
+                data[f.name] = list(item for item in f.value_from_object(instance))
         else:
             data[f.name] = f.value_from_object(instance)
+
+    print(data)
     return data
