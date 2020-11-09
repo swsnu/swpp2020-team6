a5780e948b95f870b3819301b66a31976e9370c3
ddanddan18 <bob070030@gmail.com>
2020-11-09 21:22:30 +0900

    refactor: modify roadmap field related name

--- backend/user/models.py
+++ backend/user/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     user_picture_url = models.CharField(max_length=64, default="", blank=True)
     pinned_roadmaps = models.ManyToManyField(
-        "roadmap.Roadmap", related_name="pinned_roadmap_user", blank=True
+        "roadmap.Roadmap", related_name="pinned_roadmaps_user", blank=True
     )
     liked_roadmaps = models.ManyToManyField(
-        "roadmap.Roadmap", related_name="liked_roadmap_user", blank=True
+        "roadmap.Roadmap", related_name="liked_roadmaps_user", blank=True
     )
 
     def __str__(self):
