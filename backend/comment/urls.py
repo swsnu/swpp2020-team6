410ee96192f492b7ab3dac976db1b34a5aa36443
ddanddan18 <bob070030@gmail.com>
2020-11-11 00:19:22 +0900

    feat: add create comment api (POST /api/comment/)

--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from comment import views
 
 urlpatterns = [
-    # path("", views.comment, name="comment"),
+    path("", views.comment, name="comment"),
     # path("<int:comment_id>/", views.comment_id, name="comment_id"),
 ]
