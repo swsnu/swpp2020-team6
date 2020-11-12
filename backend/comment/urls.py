ecdb03fbbc0fbf024f7ddc86d7bb21f851de05ea
ddanddan18 <bob070030@gmail.com>
2020-11-12 22:08:04 +0900

    :sparkles: Edit/Delete comment backend

--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -3,5 +3,5 @@ from comment import views
 
 urlpatterns = [
     path("", views.comment, name="comment"),
-    # path("<int:comment_id>/", views.comment_id, name="comment_id"),
+    path("<int:comment_id>/", views.comment_id, name="comment_id"),
 ]
