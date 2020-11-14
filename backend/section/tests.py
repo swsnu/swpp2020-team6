c0a85b22847f8f22550eb0cd3449bebbb6380d3a
ddanddan18 <bob070030@gmail.com>
2020-11-14 11:47:03 +0900

    :white_check_mark: section model test

--- backend/section/tests.py
+++ backend/section/tests.py
@@ -1,3 +1,20 @@
+import json
 from django.test import TestCase
+from .models import Section
+from roadmap.models import Roadmap
+from user.models import User
 
-# Create your tests here.
+
+class SectionTestCase(TestCase):
+    dump_user = {"username": "chris", "email": "chris@gmail.com", "password": "chris"}
+
+    def test_section(self):
+        user = User.objects.create_user(
+            username=self.dump_user["username"],
+            email=self.dump_user["email"],
+            password=self.dump_user["password"],
+        )
+        roadmap = Roadmap(title="roadmap title", original_author=user, author=user)
+        roadmap.save()
+        section = Section.objects.create(title="section title", roadmap=roadmap)
+        self.assertEqual(section.__str__(), section.title)
