a55721c66ad7750430ad7bb01290105bf5327fd0
gina7484 <gina7484@naver.com>
2020-11-23 09:50:15 +0900

    :recycle: Refactor: reduced complexity in progress tracking backend.

--- backend/roadmap/views.py
+++ backend/roadmap/views.py
@@ -309,98 +309,107 @@ def roadmap_id_progress(request, roadmap_id):
             return HttpResponseBadRequest()
 
         # check if the transition is valid & change progress state
-        if new_progress_state == 1:
-            if target_roadmap.progress == 3 or target_roadmap.progress == 2:
-                # progress clear(3->1) or quit(2->1)
-                target_roadmap.progress = 1
-                target_roadmap.save()
-                response_dict = {
-                    "progress_state": 1,
-                    "sections": list(
-                        {
-                            "section_id": section.id,
-                            "section_title": section.title,
-                            "tasks": list(
-                                {
-                                    "task_id": task.id,
-                                    "task_title": task.title,
-                                    "task_type": task.type,
-                                    "task_url": task.url,
-                                    "task_description": task.description,
-                                    "task_checked": task.checked,
-                                }
-                                for task in section.task_section.all()
-                            ),
-                        }
-                        for section in target_roadmap.section_roadmap.all()
-                    ),
-                }
-                return JsonResponse(response_dict)
-            else:
-                return HttpResponseBadRequest()
-        elif new_progress_state == 3:
-            if target_roadmap.progress == 2:  # progress finish
-                target_roadmap.progress = 3
-                target_roadmap.save()
-
-                response_dict = {
-                    "progress_state": 3,
-                    "sections": list(
-                        {
-                            "section_id": section.id,
-                            "section_title": section.title,
-                            "tasks": list(
-                                {
-                                    "task_id": task.id,
-                                    "task_title": task.title,
-                                    "task_type": task.type,
-                                    "task_url": task.url,
-                                    "task_description": task.description,
-                                    "task_checked": task.checked,
-                                }
-                                for task in section.task_section.all()
-                            ),
-                        }
-                        for section in target_roadmap.section_roadmap.all()
-                    ),
-                }
-                return JsonResponse(response_dict)
-            else:
-                return HttpResponseBadRequest()
-        elif new_progress_state == 2:
-            if target_roadmap.progress == 1:  # progress start
-                target_roadmap.progress = 2
-                target_roadmap.clear_section_progress()
-                target_roadmap.save()
-
-                response_dict = {
-                    "progress_state": 2,
-                    "sections": list(
-                        {
-                            "section_id": section.id,
-                            "section_title": section.title,
-                            "tasks": list(
-                                {
-                                    "task_id": task.id,
-                                    "task_title": task.title,
-                                    "task_type": task.type,
-                                    "task_url": task.url,
-                                    "task_description": task.description,
-                                    "task_checked": task.checked,
-                                }
-                                for task in section.task_section.all()
-                            ),
-                        }
-                        for section in target_roadmap.section_roadmap.all()
-                    ),
-                }
-                return JsonResponse(response_dict)
-            else:
-                return HttpResponseBadRequest()
-        else:
-            return HttpResponseBadRequest()
+        return progress_change(roadmap_id, new_progress_state)
 
     return HttpResponseNotAllowed(["PUT"])
+
+
+def progress_change(roadmap_id, new_progress_state):
+    try:
+        target_roadmap = Roadmap.objects.get(id=roadmap_id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound()
+
+    if new_progress_state == 1:
+        if target_roadmap.progress == 3 or target_roadmap.progress == 2:
+            # progress clear(3->1) or quit(2->1)
+            target_roadmap.progress = 1
+            target_roadmap.save()
+            response_dict = {
+                "progress_state": 1,
+                "sections": list(
+                    {
+                        "section_id": section.id,
+                        "section_title": section.title,
+                        "tasks": list(
+                            {
+                                "task_id": task.id,
+                                "task_title": task.title,
+                                "task_type": task.type,
+                                "task_url": task.url,
+                                "task_description": task.description,
+                                "task_checked": task.checked,
+                            }
+                            for task in section.task_section.all()
+                        ),
+                    }
+                    for section in target_roadmap.section_roadmap.all()
+                ),
+            }
+            return JsonResponse(response_dict)
+        else:
+            return HttpResponseBadRequest()
+    elif new_progress_state == 3:
+        if target_roadmap.progress == 2:  # progress finish
+            target_roadmap.progress = 3
+            target_roadmap.save()
+
+            response_dict = {
+                "progress_state": 3,
+                "sections": list(
+                    {
+                        "section_id": section.id,
+                        "section_title": section.title,
+                        "tasks": list(
+                            {
+                                "task_id": task.id,
+                                "task_title": task.title,
+                                "task_type": task.type,
+                                "task_url": task.url,
+                                "task_description": task.description,
+                                "task_checked": task.checked,
+                            }
+                            for task in section.task_section.all()
+                        ),
+                    }
+                    for section in target_roadmap.section_roadmap.all()
+                ),
+            }
+            return JsonResponse(response_dict)
+        else:
+            return HttpResponseBadRequest()
+    elif new_progress_state == 2:
+        if target_roadmap.progress == 1:  # progress start
+            target_roadmap.progress = 2
+            target_roadmap.clear_section_progress()
+            target_roadmap.save()
+
+            response_dict = {
+                "progress_state": 2,
+                "sections": list(
+                    {
+                        "section_id": section.id,
+                        "section_title": section.title,
+                        "tasks": list(
+                            {
+                                "task_id": task.id,
+                                "task_title": task.title,
+                                "task_type": task.type,
+                                "task_url": task.url,
+                                "task_description": task.description,
+                                "task_checked": task.checked,
+                            }
+                            for task in section.task_section.all()
+                        ),
+                    }
+                    for section in target_roadmap.section_roadmap.all()
+                ),
+            }
+            return JsonResponse(response_dict)
+        else:
+            return HttpResponseBadRequest()
+    else:
+        return HttpResponseBadRequest()
 
 
 def search(request):
