48c670f9c2722982ad482418b95d3dd6acaf1b26
ddanddan18 <bob070030@gmail.com>
2020-11-26 06:13:03 +0900

    :sparkles: feat: Logobutton and simple test

---
+++ frontend/src/components/LogoButton/LogoButton.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router";
+import logoImg from "../../misc/rotus.png";
+
+class LogoButton extends Component {
+  onClickLogo = () => {
+    const { history } = this.props;
+    history.push("/home");
+  };
+
+  render() {
+    return (
+      <div className="LogoButton">
+        <button type="button" id="logo-button" onClick={() => this.onClickLogo()}>
+          <img src={logoImg} alt="logo" />
+        </button>
+      </div>
+    );
+  }
+}
+
+LogoButton.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any),
+};
+export default withRouter(LogoButton);
