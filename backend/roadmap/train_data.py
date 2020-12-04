1c661e8746283489d0adb8bf7ebc4fa738df772a
gina7484 <gina7484@naver.com>
2020-12-04 20:11:54 +0900

    :bug: FIx: fixed reading csv

--- backend/roadmap/train_data.py
+++ backend/roadmap/train_data.py
@@ -2,18 +2,16 @@ import csv
 from .models import Roadmap
 
 
-def preprocess_tags():
+def preprocess_roadmaps():
     tag_sample = []
     processed_roadmaps = []
 
-    with open("tags.csv", "w", newline="") as file:
+    with open("tags.csv", "r") as file:
         reader = csv.reader(file)
         for row in reader:
             tag_sample = row
 
-    roadmaps = Roadmap.objects.select_related("id", "level", "tags").filter(
-        private=False
-    )
+    roadmaps = Roadmap.objects.prefetch_related("tags").filter(private=False)
 
     for roadmap in roadmaps:
         level = [0] * 3
@@ -21,7 +19,7 @@ def preprocess_tags():
         tags = [0] * len(tag_sample)
         tag_index = 0
 
-        for tag_item in roadmap.tags:
+        for tag_item in roadmap.tags.all():
             try:
                 tag_index = tag_sample.index(tag_item)
             except (ValueError):
@@ -29,3 +27,5 @@ def preprocess_tags():
             tags[tag_index] = 1
 
         processed_roadmaps.append({"id": roadmap.id, "level": level, "tags": tags})
+
+    print(processed_roadmaps)
