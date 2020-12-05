21abc45d5c835af6463563b072baab349f640190
ddanddan18 <bob070030@gmail.com>
2020-12-05 16:43:32 +0900

    :wrench: Seprerate backend setting files

---
+++ backend/backend/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['18.206.120.0']
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'name',
+#        'USER': 'user',
+#        'PASSWORD': 'password'
+#    }
+#}
