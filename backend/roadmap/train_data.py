ebf454d2bd7b0b8fffd92cff5edd1c323289dab6
gina7484 <gina7484@naver.com>
2020-12-06 21:04:57 +0900

    feature: K modes processing part done.
    For all the csv files, I assumed the generating functions
    will all be ran on the /swpp2020-team6/backend.
    So all the files are saved under /swpp2020-team6/backend.
    
    1. tags.csv: create a single row with currently existing tags.
    2. kmodes_input.csv: creates data for each roadmap
       (index: roadmap_id, level, tag list)
    3. clustering_result.csv: has data clustered results in a form as below.
       (index: cluster_predicted, roadmap_id)

--- backend/roadmap/train_data.py
+++ backend/roadmap/train_data.py
@@ -1,31 +1,68 @@
 import csv
 from .models import Roadmap
+import pandas as pd
+from kmodes.kmodes import KModes
+from tag.make_tag_sample import preprocess_tags
 
 
 def preprocess_roadmaps():
     tag_sample = []
     processed_roadmaps = []
 
+    # create index row
     with open("tags.csv", "r") as file:
         reader = csv.reader(file)
         for row in reader:
             tag_sample = row
 
+    tag_sample[:0] = ["roadmap_id", "level"]
+
+    # create following rows
     roadmaps = Roadmap.objects.prefetch_related("tags").filter(private=False)
+    with open("kmodes_input.csv", "w", newline="") as file:
+        writer = csv.writer(file)
 
-    for roadmap in roadmaps:
-        level = [0] * 3
-        level[roadmap.level - 1] = 1
-        tags = [0] * len(tag_sample)
-        tag_index = 0
+        # write index row
+        writer.writerow(tag_sample)
 
-        for tag_item in roadmap.tags.all().values():
-            try:
-                tag_index = tag_sample.index(tag_item["tag_name"])
-            except (ValueError):
-                continue
-            tags[tag_index] = 1
+        for roadmap in roadmaps:
+            tags = [0] * len(tag_sample)
+            tag_index = 0
 
-        processed_roadmaps.append({"id": roadmap.id, "level": level, "tags": tags})
+            for tag_item in roadmap.tags.all().values():
+                try:
+                    tag_index = tag_sample.index(tag_item["tag_name"])
+                except (ValueError):
+                    continue
+                tags[tag_index] = 1
 
-    print(processed_roadmaps)
+            writer.writerow([roadmap.id, roadmap.level] + tags)
+
+    return processed_roadmaps
+
+
+def run_kmodes():
+    km_huang = KModes(n_clusters=10, init="Huang", n_init=5, verbose=1)
+    csv_data = pd.read_csv("kmodes_input.csv")
+    input_data = csv_data.iloc[:, 1:]
+    roadmap_id = csv_data.iloc[:, 0]
+    clusters = km_huang.fit_predict(input_data)
+
+    cluster_df = pd.DataFrame(clusters)
+    cluster_df.columns = ["cluster_predicted"]
+    cluster_df["roadmap_id"] = roadmap_id
+
+    # save as csv
+    cluster_df.to_csv("clustering_result.csv", sep=",", na_reap="NaN", index=False)
+
+
+def train_process(build_tag_sample, build_roadmap_data):
+    # create tags.csv with currently existing tags
+    if build_tag_sample:
+        preprocess_tags()
+
+    # create kmodes_input.csv based on currently existing roadmaps
+    if build_roadmap_data:
+        preprocess_roadmaps()
+
+    run_kmodes()
