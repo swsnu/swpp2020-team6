6bd785ae6deceb90a62deffc083dbd8704b7f5d5
ddanddan18 <bob070030@gmail.com>
2020-12-09 05:13:51 +0900

    :beers: Skeleton code for inference recommendation

---
+++ backend/roadmap/recommend/inference_recommend.py
@@ -0,0 +1,13 @@
+
+def top_n_cluster(like_roadmap, pin_roadmap, n=3):
+    # TODO: like_roadmap, pin_roadmap의 index를 보고
+    #  return top popular n cluster index
+    pass
+
+def top_roadmaps(clusters, roadmaps, n_per_cluster=4):
+    # TODO: #comment + #like + #pin으로
+    #  return top popular n roadmaps per each cluster
+    pass
+
+# TODO: roadmap models에 roadmap, id, cluster정보 같은 내용들로 바꿔주는 함수 만들어서,
+#  이런데다가 param전달할 때 쓰기
